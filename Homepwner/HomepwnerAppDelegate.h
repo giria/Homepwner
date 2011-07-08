700c44d4b2ee8cd9f469ad0fcd893cc99c74882e
joan <joan@giria.com>
2011-07-08 09:18:34 +0200

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Joan Barrull Ribalta on 7/8/11.
+//  Copyright 2011 giria.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Joan Barrull Ribalta on 7/8/11.
+//  Copyright 2011 giria.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
